d7105edecab4611569a7bb21ec123617879f2173
daersh <daersh@naver.com>
2024-03-15 10:38:52 +0900

    :+1: [Add] 3장 Boolean

---
+++ JS/01_javascript_core/02_variable/01_data-type/03_boolean.js
@@ -0,0 +1,18 @@
+/*03. Boolean type*/
+
+var bool = true;
+console.log(bool);
+console.log(typeof bool);
+var bool2 = false;
+console.log(bool2);
+console.log(typeof bool2);
+
+/**
+ * Boolean type은 추가적으로 세 가지 특별한 값도 표현할 수 있다.
+ * true: true
+ * false: false
+ * null: null
+ * undefined
+ * null: undefined
+ * undefined: undefined
+ */
